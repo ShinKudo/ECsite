1393200e62605d507c809750c60ad185a19412b8
Shin Kudo <ShinKudo@SKs-MacBook-Pro.local>
2017-10-20 16:00:05 +0900

    Revert "BootStrap適用"
    This reverts commit 17c1af429772b15f0f761a38e4d212d8a324cf3b.

--- Gemfile
+++ Gemfile
@@ -34,8 +34,6 @@ gem 'jbuilder', '~> 2.5'
 # gem 'capistrano-rails', group: :development
 
 gem 'devise'
-gem 'bootstrap', '~>4.0.0.beta'
-gem 'jquery-rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
   before_action :set_book, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!          
+  before_action :authenticate_user!              
 
   # GET /books
   # GET /books.json
@@ -70,6 +70,6 @@ class BooksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def book_params
-      params.require(:book).permit(:title, :author, :published_on, :showing, :price, tag_ids: [])
+      params.require(:book).permit(:title, :author, :published_on, :showing, :price)
     end
 end
