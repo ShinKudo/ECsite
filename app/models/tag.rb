17c1af429772b15f0f761a38e4d212d8a324cf3b
Shin Kudo <ShinKudo@SKs-MacBook-Pro.local>
2017-10-20 15:59:47 +0900

    BootStrap適用

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ApplicationRecord
   validates :name, presence: true
-  has_many :taggings, dependent: :destroy
-  has_many :bokks, dependent: :destroy
+  has_many :taggings
+  has_many :books, through: :taggings
 end
