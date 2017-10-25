75bc9fdac0dcb42162167f5a0f574359c768057d
Shin Kudo <ShinKudo@SKs-MacBook-Pro.local>
2017-10-25 18:02:33 +0900

    メール送信機能実装(修正版)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,17 +1,10 @@
 class ProductsController < ApplicationController
-  before_action :set_book, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
 
-   def index
+  def index
     @books = Book.all
-   end
+  end
  
-   def show
-   end
- 
-   private
-   # Use callbacks to share common setup or constraints between actions.
-   def set_book
-     @book = Book.find(params[:id])
-   end 
-end
+  def show
+    @book = Book.find(params[:id])
+  end
