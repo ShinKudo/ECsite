44d343e1f3cf321851c06064fb85a00b9efcdafe
Shin Kudo <ShinKudo@SKs-MacBook-Pro.local>
2017-10-27 14:12:32 +0900

    no message

--- db/migrate/20171025043845_create_orders.rb
+++ db/migrate/20171025043845_create_orders.rb
@@ -1,8 +1,8 @@
 class CreateOrders < ActiveRecord::Migration[5.1]
   def change
     create_table :orders do |t|
-      t.references :user_id, foreign_key: true
-      t.string :book_id
+      t.references :user, foreign_key: true
+      t.string :book, foreign_key: true
       t.string :references
       t.string :email
       t.timestamps
